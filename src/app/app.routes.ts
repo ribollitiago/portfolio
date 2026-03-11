f7db8923617a71b1b5dd754bb7018fa627e97458
Tiago Ribolli <ribollitiago@gmail.com>
2026-03-10 23:09:50 -0300

    sidenav and pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BudgetComponent } from './pages/budget/budget.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'orcamento', component: BudgetComponent },
+  { path: '**', redirectTo: '' }
+];
